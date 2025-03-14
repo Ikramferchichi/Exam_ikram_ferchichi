f6f5cd5abc3e3d53cef7db4f004bc8f5fbc36f43
Ikramferchichi <ferchichiikram08@gmail.com>
2025-03-14 11:15:17 +0100

    resolution 2éme conflit de main.py

--- captures/main.py
+++ captures/main.py
@@ -6,18 +6,25 @@ def somme(L):
   return s
 
 my_list=[1,5,7]
+
 som=somme(my_list)
 print('la somme est =',som)
 my_list=[1,5,7]
+
+
 if my_list:
     print('la somme est:',sum(my_list))
     print('le max est:',max(mylist))
     print('le min est:',min(mylist))
 else:
     print('iste vide')
+<<<<<<< HEAD
 
 
 print('la somme est:',sum(my_list))
 
 =======
+>>>>>>> feature_list
+=======
+
 >>>>>>> feature_list
